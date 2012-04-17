4ee20b7ccaca1ceed1bc11555a9e10ea324c381a
Dave Ingram <dave@dmi.me.uk>
2012-04-17 23:28:45 +0100

    Rewrote compiler to use dispatch tables for easy extending

--- lessons/2/mus-compiler.js
+++ lessons/2/mus-compiler.js
@@ -1,48 +1,55 @@
-var endTime = function (t, musexpr) {
-  if (musexpr.tag === 'note') {
-    return t + musexpr.dur;
-  } else if (musexpr.tag === 'seq') {
-    return endTime(
-        endTime(t, musexpr.left),
-        musexpr.right
-        );
-  } else if (musexpr.tag === 'par') {
-    return Math.max(
-        endTime(t, musexpr.left),
-        endTime(t, musexpr.right)
-        );
-  }
+var endTime_dispatch = {
+  'note': function(t,e) { return t + e.dur; },
+  'seq':  function(t,e) { return endTime(endTime(t, e.left), e.right); },
+  'par':  function(t,e) { return Math.max(endTime(t, e.left), endTime(t, e.right)); },
 };
 
-var compile_ = function (start, musexpr) {
-  if (musexpr.tag === 'note') {
-    return [ {
-      tag: 'note',
-        pitch: musexpr.pitch,
-        start: start,
-        dur: musexpr.dur
-    } ];
-  } else if (musexpr.tag === 'seq') {
-    return compile_(
-        start,
-        musexpr.left
-        ).concat(compile_(
-            endTime(start, musexpr.left),
-            musexpr.right
-            ));
-  } else {
-    return compile_(
-        start,
-        musexpr.left
-        ).concat(compile_(
-            start,
-            musexpr.right
-            ));
+var endTime = function (start, musexpr) {
+  if (!musexpr.tag in endTime_dispatch) {
+    throw 'Unrecognised music tag "' + musexpr.tag + '"';
+    return start;
   }
+  return endTime_dispatch[musexpr.tag](start, musexpr);
+};
+
+var compile_dispatch = {
+  'note': function(s, i, o) {
+    o.push({
+      tag: 'note',
+        pitch: i.pitch,
+        start: s,
+        dur:   i.dur
+    });
+    return s + i.dur;
+  },
+
+  'seq': function(s, i, o) {
+    s = compile_(s, i.left, o);
+    s = compile_(s, i.right, o);
+    return s;
+  },
+
+  'par': function(s, i, o) {
+    s = Math.max(
+      compile_(s, i.left, o),
+      compile_(s, i.right, o)
+    );
+    return s;
+  },
+};
+
+var compile_ = function (start, musexpr, out) {
+  if (!musexpr.tag in compile_dispatch) {
+    throw 'Unrecognised music tag "' + musexpr.tag + '"';
+    return start;
+  }
+  return compile_dispatch[musexpr.tag](start, musexpr, out);
 };
 
 var compile = function (musexpr) {
-  return compile_(0, musexpr);
+  var out = [];
+  compile_(0, musexpr, out);
+  return out;
 };
 
 module.exports = {compile: compile};
