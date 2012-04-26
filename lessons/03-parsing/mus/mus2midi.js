afccacfe3ab6bc31e3ef07a5bc1f188bd26815b4
Dave Ingram <dmi@teamgroupspaces.com>
2012-04-26 10:53:36 +0100

    Whitespace fix

--- lessons/03-parsing/mus/mus2midi.js
+++ lessons/03-parsing/mus/mus2midi.js
@@ -29,60 +29,60 @@ var durToTicks = function(d) {
 
 var _dispatch = {
 
-  compile: {
+	compile: {
 
-    'note': function(s, i, o) {
+		'note': function(s, i, o) {
 			var start = s,
-				end = s + (i.len ? Math.floor(512 / i.len) : durToTicks(i.dur));
-      o.push({
-        tag: 'noteOn',
+			end = s + (i.len ? Math.floor(512 / i.len) : durToTicks(i.dur));
+			o.push({
+				tag: 'noteOn',
 				time: start,
 				pitch: i.pitch,
-      });
-      o.push({
-        tag: 'noteOff',
+			});
+			o.push({
+				tag: 'noteOff',
 				time:  end,
 				pitch: i.pitch,
 			});
-      return end;
-    },
+			return end;
+		},
 
-    'rest': function(s, i, o) {
+		'rest': function(s, i, o) {
 			return s + (i.len ? Math.floor(512 / i.len) : durToTicks(('dur' in i) ? i.dur : i.duration));
-    },
+		},
 
-    'tempo': function(s, i, o) {
+		'tempo': function(s, i, o) {
 			o.push({
 				tag: 'tempo',
-				time: s,
-				bpm: i.tempo * i.base/4,
+			time: s,
+			bpm: i.tempo * i.base/4,
 			});
 			return s;
-    },
+		},
 
-    'seq': function(s, i, o) {
-      return compile_(
-        compile_(s, i.left, o),
-        i.right,
-        o
-      );
-    },
+		'seq': function(s, i, o) {
+			return compile_(
+					compile_(s, i.left, o),
+					i.right,
+					o
+					);
+		},
 
-    'par': function(s, i, o) {
-      return Math.max(
-        compile_(s, i.left, o),
-        compile_(s, i.right, o)
-      );
-    },
+		'par': function(s, i, o) {
+			return Math.max(
+					compile_(s, i.left, o),
+					compile_(s, i.right, o)
+					);
+		},
 
-    'repeat': function(s, i, o) {
-      for (var n = 0; n < i.count; ++n) {
-        s = compile_(s, i.section, o);
-      }
+		'repeat': function(s, i, o) {
+			for (var n = 0; n < i.count; ++n) {
+				s = compile_(s, i.section, o);
+			}
 			return s;
-    },
+		},
 
-  },
+	},
 
 };
 
